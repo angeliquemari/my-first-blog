c623eaf9fb52a7104fd611fdd72d2157f1ae2011
Angelique Nehmzow <angelique@nehmzow.com>
2019-03-27 23:04:39 -0700

    adding login/out method

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .models import CommentTest
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 import pdb
 
 def post_list(request):
@@ -14,6 +15,7 @@ def post_detail(request, pk):
     comments = CommentTest.objects.filter(post__id=pk).order_by('comment_date')
     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -26,6 +28,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -39,15 +42,18 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
