1d48e46600b086f80a2b6dca2778fdffa9d9d785
Angelique Nehmzow <angelique@nehmzow.com>
2019-03-29 00:18:55 -0700

    removing old comment model adding new one, with approval system and adding option

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
-from .models import CommentTest
+from .models import Post, Comment
+# from .models import CommentTest
 
 admin.site.register(Post)
-admin.site.register(CommentTest)
+# admin.site.register(CommentTest)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -14,14 +14,31 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
-class CommentTest(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    comment = models.CharField(max_length=1000)
-    comment_date = models.DateTimeField(default=timezone.now)
+# class CommentTest(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     comment = models.CharField(max_length=1000)
+#     comment_date = models.DateTimeField(default=timezone.now)
+#
+#     def __str__(self):
+#         return self.comment
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
 
     def __str__(self):
-        return self.comment
+        return self.text
